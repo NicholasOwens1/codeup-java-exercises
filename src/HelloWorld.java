a3b76f86e3c48e520d879be935454ea20219e1b1
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-04-09 21:55:08 -0500

    java exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,24 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        System.out.print("Hello World");
+        short myFavoriteNumber = 86;
+        System.out.print(myFavoriteNumber);
+        String myString = "Behold my string.";
+        System.out.println(myString);
+//        int x = 4;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+
+
+
     }
+
 }
