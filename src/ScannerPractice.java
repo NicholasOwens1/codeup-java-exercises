4549b3cb3d966ddb7664630883b25a38e6b0de25
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-04-10 10:54:39 -0500

    Scanner

---
+++ src/ScannerPractice.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ScannerPractice {
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter name");
+        String name = scanner.next();
+        System.out.println("Hello, " +name);
+    }
+}
