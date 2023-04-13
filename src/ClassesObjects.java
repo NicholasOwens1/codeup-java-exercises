eb898786b27149cbd8045d82e4ff1bd999256249
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-04-13 09:04:52 -0500

    Animal Exercise

--- src/ClassesObjects.java
+++ src/ClassesObjects.java
@@ -1,23 +1,30 @@
 //defining a Person class.
-public class Person {
+public class ClassesObjects {
+//      creating 5 fields for the class.
     public String firstName;
     public String lastName;
     public String city;
     public String state;
     public int zipCode;
-
+//      creating an instance method inside the class
     public String data(){
         return "My name is " + firstName + " " + lastName + ". " + "I am from " + city + " " + state + ". My zip code is " + zipCode + ".";
      }
-
+//      creating a main method with a new instantiation of the Person class with 5 variables. Then outputting the data method with the instantiation's variables
     public static void main(String[] args) {
-        Person Nick = new Person();
+        ClassesObjects Nick = new ClassesObjects();
         Nick.lastName = "Owens";
         Nick.firstName = "Nick";
         Nick.city = "San Antonio";
         Nick.state = "Texas";
         Nick.zipCode = 78204;
-        System.out.println(Nick.data());
+        ClassesObjects Navia = new ClassesObjects();
+        Navia.lastName = "Alexander";
+        Navia.firstName = "Navia";
+        Navia.city = "San Antonio";
+        Navia.state = "Texas";
+        Navia.zipCode = 78204;
+        System.out.println(Navia.data());
     }
 }
     // make 5 fields for the class, or instance variables. First Name, last name, city, state, zip code.
@@ -27,3 +34,5 @@ public class Person {
 
         // instantiate a new Person object inside a main method. Give it 5 variables . Output the
 // data for the newly instantiated object using the instance method you created.
+//
+// Create a new Person class inside the main method with new variable values. Output it.
