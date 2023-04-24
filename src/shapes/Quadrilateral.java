8b5a47965c1bfe9dd6fbeadc27fffb0a07f71cf8
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-04-24 10:07:44 -0500

    interfaceAbstract exercise #1 & #2

---
+++ src/shapes/Quadrilateral.java
@@ -0,0 +1,5 @@
+package shapes;
+
+public abstract class Quadrilateral extends Shape implements Measurable {
+
+}
--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,45 +1,46 @@
 package shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
-    public int getWidth() {
-        return width;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-
-    public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
-    }
+//    protected int length;
+//    protected int width;
+//    public int getWidth() {
+//        return width;
+//    }
+//
+//    public int getLength() {
+//        return length;
+//    }
+//
+//
+//    public Rectangle(int length, int width) {
+//        this.length = length;
+//        this.width = width;
+//    }
 
 //    public int getlengthWidth(int length, int width) {
 //        return this.length;
 //        return this.width;
 //    }
 
-    public int getArea() { return getLength() * getWidth();}
-
-    public int getPerimeter() {
-               return 2 * getLength() + 2 * getWidth();
-
-    }
+//    public int getArea() { return getLength() * getWidth();}
+//
+//    public int getPerimeter() {
+//               return 2 * getLength() + 2 * getWidth();
+//
+//    }
 
     public static class Square extends Rectangle{
 
-        public Square(int side) {
-            super(side, side);
-        }
-        public int getArea() {
-            return 4 * getWidth();
-        }
-
-        public int getPerimeter() {
-            return getWidth() ^ 2;
-        }
+//        public Square(int side) {
+//            super(side, side);
+//        }
+//        public int getArea() {
+//            return 4 * getWidth();
+//        }
+//
+//        public int getPerimeter() {
+//            return getWidth() ^ 2;
+//        }
     }
 }
+
