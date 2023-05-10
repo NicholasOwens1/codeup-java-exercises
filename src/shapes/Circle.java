804fd8323af9f31d6318f4b00567399007cd76f6
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-05-10 13:50:34 -0500

    circle and circleApp

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,32 @@
 package shapes;
 
+import util.Input;
+
 public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * (radius * radius);
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public static void outputCircleInfo(){
+        double radius = new Input().getDouble("Please enter the radius of the circle:");
+        Circle circle = new Circle(radius);
+        System.out.printf("The area of your circle is: %.2f%n", circle.getArea());
+        System.out.printf("The circumference of your circle is: %.2f%n", circle.getCircumference());
 }
---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,12 @@
+package shapes;
+
+public class CircleApp {
+    public static void main(String[] args) {
+
+        public static void outputCircleInfo(){
+            double radius = new Input().getDouble("Please enter the radius of the circle:");
+            Circle circle = new Circle(radius);
+            System.out.printf("The area of your circle is: %.2f%n", circle.getArea());
+            System.out.printf("The circumference of your circle is: %.
+    }
+}
